667adac86514033bea0bffe44f29cf702cdedb87
Random_Civvy <vomalexander@gmail.com>
2017-07-18 23:55:36 -0700

    'Clone' command now deep clones

--- popstcl-core/src/cmds/misc.rs
+++ popstcl-core/src/cmds/misc.rs
@@ -6,6 +6,6 @@ pub struct Clone;
 impl Cmd for Clone {
     fn execute(&self, stack: &mut Stack, args: Vec<CIR>) -> Result<ExecSignal, ExecErr> {
         exact_args!(args, 1);
-        Ok(ExecSignal::NextInstruction(Some(args[0].value.inner_clone().into())))
+        Ok(ExecSignal::NextInstruction(Some(args[0].value.deep_clone())))
     }
 }
